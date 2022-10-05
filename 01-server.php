b69ae66608eb28c29906c7ed07c4ac3d0c323601
zuzubaidillah <zuzubaidillah@gmail.com>
2022-10-05 08:20:51 +0700

    first commit

---
+++ 01-server.php
@@ -0,0 +1,22 @@
+<?php
+// echo "<pre>";
+// var_dump($_SERVER);
+// die();
+?>
+<html>
+
+<body>
+    <h1>$_SERVER</h1>
+
+    <table border="1">
+        <?php foreach ($_SERVER as $key => $value) { ?>
+        <tr>
+            <td><?= $key ?></td>
+            <td><?= $value ?></td>
+        </tr>
+        <?php } ?>
+    </table>
+
+</body>
+
+</html>
